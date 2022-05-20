26a4f5981bd1b2373d8588e39978798b619f07c2
amy.molloy <amy.molloy.13@icloud.com>
2022-05-20 18:47:54 +0100

    Initial Commit

---
+++ ConsoleGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ConsoleGame
+//
+//  Created by Amy Molloy on 20/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
